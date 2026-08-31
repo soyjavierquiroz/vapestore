8e36df46110303f64e9f623b49004a611f288cdb
Javier Quiroz <javierquiroztv@gmail.com>
2026-08-31 21:33:52 +0000

    feat: brand WordPress login screen

--- plugins/vapestore-core/vapestore-core.php
+++ plugins/vapestore-core/vapestore-core.php
@@ -63,3 +63,49 @@ function vapestore_core_check_svg_filetype_and_ext( $file_type_ext, $file, $file
 	return $file_type_ext;
 }
 add_filter( 'wp_check_filetype_and_ext', 'vapestore_core_check_svg_filetype_and_ext', 10, 4 );
+
+/**
+ * Enqueue branded styles for the native WordPress login screen.
+ *
+ * @return void
+ */
+function vapestore_core_login_enqueue_styles() {
+	$asset_path = plugin_dir_path( __FILE__ ) . 'assets/css/login.css';
+	$asset_url  = plugin_dir_url( __FILE__ ) . 'assets/css/login.css';
+	$version    = file_exists( $asset_path ) ? (string) filemtime( $asset_path ) : '0.1.0';
+
+	wp_enqueue_style( 'vapestore-core-login', $asset_url, array(), $version );
+
+	$custom_logo_id = (int) get_theme_mod( 'custom_logo' );
+	$logo_url       = $custom_logo_id ? wp_get_attachment_image_url( $custom_logo_id, 'full' ) : '';
+
+	if ( $logo_url ) {
+		$custom_css = sprintf(
+			'.login h1 a{background-image:url("%s");}',
+			esc_url_raw( $logo_url )
+		);
+
+		wp_add_inline_style( 'vapestore-core-login', $custom_css );
+	}
+}
+add_action( 'login_enqueue_scripts', 'vapestore_core_login_enqueue_styles' );
+
+/**
+ * Point the native login logo link to the site home page.
+ *
+ * @return string
+ */
+function vapestore_core_login_header_url() {
+	return home_url( '/' );
+}
+add_filter( 'login_headerurl', 'vapestore_core_login_header_url' );
+
+/**
+ * Use the site name for the native login logo accessible text.
+ *
+ * @return string
+ */
+function vapestore_core_login_header_text() {
+	return get_bloginfo( 'name' );
+}
+add_filter( 'login_headertext', 'vapestore_core_login_header_text' );
